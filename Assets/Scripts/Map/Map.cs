ec4414501cc109244f642d3fef19f1fae5766642
Kaplc <755536175@qq.com>
2023-09-11 23:10:22 +0800

    轨迹和移动手感优化

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Pos
+{
+    public int x;
+    public int z;
+
+    public Pos(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+}
+
+
 public class Map : MonoBehaviour
 {
+    // 对角线长度
+    private float diagonal = 0.35921f;
+    
     // 砖块深浅颜色
     public Color color = new Color(103 / 255f, 85 / 255f, 127 / 255f);
     public Color depthColor = new Color(90 / 255f, 77 / 255f, 115 / 255f);
     public Color wallColor = new Color(82 / 255f, 59 / 255f, 102 / 255f);
 
     // 砖块坐标数据集合
-    public List<GameObject[]> tilePos = new List<GameObject[]>();
-
-    // Start is called before the first frame update
-    void Start()
+    public static List<GameObject[]> TilePos => tilePos;
+    private static List<GameObject[]> tilePos = new List<GameObject[]>();
+    
+    void Awake()
     {
         CreatMapItem();
     }
@@ -23,29 +39,29 @@ public class Map : MonoBehaviour
     {
     }
 
-    public void CreatMapItem()
+    private void CreatMapItem()
     {
         for (int j = 0; j < 10; j++)
         {
             GameObject[] OddTiles = new GameObject[5];
-            // 奇数行
+            // 偶数行
             for (int i = 0; i < 5; i++)
             {
                 GameObject tile = Instantiate(Resources.Load<GameObject>("Tile"), new Vector3(0, 0, 0), Quaternion.Euler(-90, 45, 0), transform);
                 // 每个方块偏移0.35
-                tile.transform.position = new Vector3(0.35f + i * 0.35f, 0, j * 0.35f);
+                tile.transform.position = new Vector3(diagonal + i * diagonal, 0, j * diagonal);
                 // rgb数值/255f
                 tile.GetComponent<MeshRenderer>().material.color = depthColor;
                 tile.transform.Find("normal_a2").GetComponent<MeshRenderer>().material.color = depthColor;
                 // 命名
-                tile.name = $"({j*2},{i})";
+                tile.name = $"({j * 2},{i})";
 
                 OddTiles[i] = tile;
             }
 
             tilePos.Add(OddTiles);
 
-            // 偶数行 
+            // 奇数行 
             GameObject[] evenTiles = new GameObject[6];
             for (int i = 0; i < 6; i++)
             {
@@ -54,7 +70,7 @@ public class Map : MonoBehaviour
                 {
                     // 墙
                     tile = Instantiate(Resources.Load<GameObject>("Wall"), new Vector3(0, 0, 0), Quaternion.Euler(-90, 45, 0), transform);
-                    tile.transform.position = new Vector3(0.175f + i * 0.35f, 0, 0.175f + j * 0.35f);
+                    tile.transform.position = new Vector3(diagonal/2 + i * 0.35f, 0, diagonal/2 + j * diagonal);
                     tile.GetComponent<MeshRenderer>().material.color = color;
                 }
                 else
@@ -62,12 +78,12 @@ public class Map : MonoBehaviour
                     // 砖块
                     tile = Instantiate(Resources.Load<GameObject>("Tile"), new Vector3(0, 0, 0), Quaternion.Euler(-90, 45, 0), transform);
                     // 先偏移0.175f
-                    tile.transform.position = new Vector3(0.175f + i * 0.35f, 0, 0.175f + j * 0.35f);
+                    tile.transform.position = new Vector3(diagonal/2 + i * diagonal, 0, diagonal/2 + j * diagonal);
                     tile.GetComponent<MeshRenderer>().material.color = color;
                     tile.transform.Find("normal_a2").GetComponent<MeshRenderer>().material.color = color;
                 }
 
-                tile.name = $"({j*2+1},{i})";
+                tile.name = $"({j * 2 + 1},{i})";
                 evenTiles[i] = tile;
             }
 
---
+++ Assets/Scripts/Object/Player.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int Speed;
+
+    public Pos oldPos = new Pos();
+    public Pos pos = new Pos(3, 2);
+    private Vector3 mapPos;
+
+    private Color depthColor = new Color(235 / 255f, 99 / 255f, 144 / 255f);
+    private Color color = new Color(228 / 255f, 93 / 255f, 137 / 255f);
+
+    private float cd = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 地图坐标位置赋给player
+        mapPos = Map.TilePos[pos.x][pos.z].transform.position;
+        transform.position = new Vector3(mapPos.x, 0.254f / 2, mapPos.z);
+        transform.rotation = Quaternion.Euler(new Vector3(0, -45, 0));
+    }
+
+    private void ResetPos()
+    {
+        pos.x = 3;
+        pos.z = 2;
+        mapPos = Map.TilePos[3][2].transform.position;
+        transform.position = new Vector3(mapPos.x, 0.254f / 2, mapPos.z);
+        transform.rotation = Quaternion.Euler(new Vector3(0, -45, 0));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ResetPos();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            cd += Time.deltaTime;
+            if (cd <= 0.1f) return;
+            cd = 0;
+
+            oldPos = pos;
+            // 向左
+            pos.x++;
+            // 奇数行z-1
+            if (pos.x % 2 == 0)
+            {
+                pos.z--;
+            }
+
+            // 边界判断
+            if (!MapLimit()) pos = oldPos;
+            // 每次移动创建轨迹
+            CreateMark();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            cd += Time.deltaTime;
+            if (cd <= 0.1f) return;
+            cd = 0;
+
+            oldPos = pos;
+            // 向右
+            pos.x++;
+            // 奇数行z-1
+            if (pos.x % 2 != 0)
+            {
+                pos.z++;
+            }
+
+            if (!MapLimit()) pos = oldPos;
+            CreateMark();
+        }
+
+        mapPos = Map.TilePos[pos.x][pos.z].transform.position;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(mapPos.x, transform.position.y, mapPos.z), Time.deltaTime * Speed);
+        // transform.position = new Vector3(mapPos.x, transform.position.y, mapPos.z);
+        
+    }
+
+    private bool MapLimit()
+    {
+        // 奇数行的0和 5不能
+        if (pos.x % 2 != 0 && pos.z == 0 || pos.z == 5)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void CreateMark()
+    {
+        // 获取底部砖块
+        GameObject tile = Map.TilePos[oldPos.x][oldPos.z].transform.Find("normal_a2").gameObject;
+
+        if (pos.x % 2 == 0)
+        {
+            // 偶数行深色
+            tile.GetComponent<MeshRenderer>().material.color = depthColor;
+        }
+        else
+        {
+            tile.GetComponent<MeshRenderer>().material.color = color;
+        }
+    }
+}
